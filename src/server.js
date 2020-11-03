4c192a28ea9a9c37f7887562837466b4843124b4
dhruvshaarmaa <61281854+dhruvshaarmaa@users.noreply.github.com>
2020-11-03 22:48:34 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.use("/api/posts",postsRoute);
 app.use("/api/comments",commentsRoute);
 app.use("/",express.static(__dirname+"/public"));
 
-db.sync()
+db.sync({force:true})
     .then(()=>{
         app.listen(port,()=>{
             console.log(`Server started on http://localhost:${port}`);
